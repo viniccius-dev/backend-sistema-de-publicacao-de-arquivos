a7de09239f0e1db41e8d82f6609022548bbc0225
Vinícius <santsmarcos81@gmail.com>
2024-11-05 09:48:34 -0300

    AppError e configuracao do knex

--- src/server.js
+++ src/server.js
@@ -1,9 +1,29 @@
+require("express-async-errors");
+
 const express = require("express");
+
+const AppError = require("./utils/AppError");
 
 (async () => {
     const app = express();
 
     app.use(express.json());
+
+    app.use((error, request, response, next) => {
+        if(error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: "Erro",
+                message: error.message
+            });
+        }
+
+        console.error(error);
+
+        return response.status(500).json({
+            status: "Erro",
+            message: "Erro interno do servidor"
+        });
+    });
 
     const PORT = 3333;
     app.listen(PORT, () => console.log(`Server is running ${PORT}`));
